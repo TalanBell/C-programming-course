5c98ed53bb20c89d138003b67487fe08b8f120ca
Mark Clifford <45853546+TalanBell@users.noreply.github.com>
2019-02-19 18:37:18 +0000

    Add files via upload

---
+++ ForLoopExample.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    unsigned long long sum = 0LL;  // stores sum of integers
+    unsigned int count = 0;        // number of integers to be summed
+
+    printf("\nEnter the number of integers to be summed: ");
+    scanf(" %u", &count);
+
+    // sum integers from 1 to count
+    for(unsigned int i = 1; i <= count; ++i)
+        sum += i;
+
+    printf("\nTotal of the first %u numbers is %u\n", count, sum);
+
+
+    return 0;
+}
