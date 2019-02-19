665a48385be254ac0ca1104868e7cfc93e5b0f7c
Mark Clifford <45853546+TalanBell@users.noreply.github.com>
2019-02-19 17:13:00 +0000

    Update Weekly_Pay.c
    Replacing variables with constants and using += operator

--- Weekly_Pay.c
+++ Weekly_Pay.c
@@ -4,58 +4,46 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-float basic_rate = 12.00;
-float overtime_rate = 1.5;
-float taxrate_low = .15;
-float taxrate_medium = .20;
-float taxrate_high = .25;
-float overtime_hours = 40.0;
+#define BASIC_RATE 12.00
+#define OVERTIME_RATE 1.5
+#define TAXRATE_LOW .15
+#define TAXRATE_MED .20
+#define TAXRATE_HIGH .25
+#define OVERTIME_HOURS 40
 
 int main()
 {
-    float hours_worked, gross_pay, net_pay;
-    float tax_low = 0;
-    float tax_medium = 0;
-    float tax_high = 0;
-    float tax_total;
+    float hours_worked, gross_pay, net_pay, tax_total;
 
     printf("Weekly Pay Calculation.\nPlease enter the number of hours worked: ");
     scanf("%f", &hours_worked);
 
     /// Gross Pay calculation
-    if (hours_worked > overtime_hours)
+    if (hours_worked > OVERTIME_HOURS)
     {
-        float overtime = hours_worked - overtime_hours;
-        gross_pay = (basic_rate * overtime_hours) + (basic_rate * overtime_rate * overtime);
+        gross_pay = (BASIC_RATE * OVERTIME_HOURS) + (BASIC_RATE * OVERTIME_RATE * (hours_worked - OVERTIME_HOURS));
     }
     else
-        gross_pay = basic_rate * hours_worked;
+        gross_pay = BASIC_RATE * hours_worked;
     printf("\nGross pay = $%.2f\n", gross_pay);
 
     /// Tax calculations
     if (gross_pay <= 300)
     {
-        tax_low = gross_pay * taxrate_low;
-//        printf("Tax upto $300 = $%.2f\n", tax_low);
+        tax_total = gross_pay * TAXRATE_LOW;
     }
-    else if (gross_pay <= 450)
+    else if (gross_pay > 300 && gross_pay <= 450)
     {
-        int pay_medium = gross_pay - 300;
-        tax_low = 300 * taxrate_low;
-        tax_medium = pay_medium * taxrate_medium;
-//        printf("Tax upto $300 = $%.2f. Tax $301-$450 = $%.2f.\n", tax_low, tax_medium);
+        tax_total = 300 * TAXRATE_LOW;
+        tax_total += (gross_pay - 300) * TAXRATE_MED;
     }
     else
     {
-        int pay_high = gross_pay - 450;
-        tax_low = 300 * taxrate_low;
-        tax_medium = 150 * taxrate_medium;
-        tax_high = pay_high * taxrate_high;
-//        printf("Tax upto $300 = $%.2f. Tax $301-$450 = $%.2f. Tax over $450 = $%.2f.\n", tax_low, tax_medium, tax_high);
-
+        tax_total = 300 * TAXRATE_LOW;
+        tax_total += 150 * TAXRATE_MED;
+        tax_total += (gross_pay - 450) * TAXRATE_HIGH;
     }
 
-    tax_total = tax_low + tax_medium + tax_high;
     printf("Total tax = $%.2f\n", tax_total);
 
     /// Net Pay calculation
